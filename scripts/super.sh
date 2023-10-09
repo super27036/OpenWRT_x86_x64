1751bec866da1043fa81a2405b50d0c4910cd6f2
super27036 <144255894+super27036@users.noreply.github.com>
2023-10-10 01:20:24 +0800

    Update super.sh

--- scripts/super.sh
+++ scripts/super.sh
@@ -166,10 +166,20 @@ chmod -R a+x package/base-files/files/root/5GModem
 svn export https://github.com/Siriling/OpenWRT-MyConfig/trunk/configs/general/etc/crontabs package/base-files/files/etc/crontabs
 
 # 修改默认IP地址
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
 # 修改主机名字
-sed -i 's/OpenWrt/5G CPE/g' package/base-files/files/bin/config_generate
+# sed -i 's/OpenWrt/5G CPE/g' package/base-files/files/bin/config_generate
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+# Modify default hosename
+sed -i 's/OpenWrt/SUPERouter/g' package/base-files/files/bin/config_generate
+# Password
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/$1$S2TRFyMU$E8fE0RRKR0jNadn3YLrSQ0:18690:0:99999:7:::/g' package/lean/default-settings/files/zzz-default-settings
+# Disable ipv6
+sed -i 's/def_bool y/def_bool n/g' config/Config-build.in
+
 
 # 修改版本号
 # sed -i 's/R22.3.3/R22.3.3定制版/g' package/lean/default-settings/files/zzz-default-settings
