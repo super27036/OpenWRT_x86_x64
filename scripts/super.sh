52d32340333b465bf477a5996b5bfea3c20dcad2
super27036 <144255894+super27036@users.noreply.github.com>
2023-10-10 20:59:14 +0800

    Update super.sh

--- scripts/super.sh
+++ scripts/super.sh
@@ -171,6 +171,12 @@ sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generat
 # 修改主机名字
 sed -i 's/OpenWrt/SUPERouter/g' package/base-files/files/bin/config_generate
 
+# 修改登录密码
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/$1$S2TRFyMU$E8fE0RRKR0jNadn3YLrSQ0:18690:0:99999:7:::/g' package/lean/default-settings/files/zzz-default-settings
+
+# 关掉ipv6
+sed -i 's/def_bool y/def_bool n/g' config/Config-build.in
+
 # 修改版本号
 # sed -i 's/R22.3.3/R22.3.3定制版/g' package/lean/default-settings/files/zzz-default-settings
 # sed -i 's/OpenWrt/R5S/g' package/lean/default-settings/files/zzz-default-settings
