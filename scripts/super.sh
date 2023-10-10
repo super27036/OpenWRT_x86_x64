b5b74f3773855490373b2ddec54518950b001ddd
super27036 <144255894+super27036@users.noreply.github.com>
2023-10-10 14:08:51 +0800

    Update super.sh

--- scripts/super.sh
+++ scripts/super.sh
@@ -166,10 +166,10 @@ chmod -R a+x package/base-files/files/root/5GModem
 svn export https://github.com/Siriling/OpenWRT-MyConfig/trunk/configs/general/etc/crontabs package/base-files/files/etc/crontabs
 
 # 修改默认IP地址
- sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
 # 修改主机名字
-# sed -i 's/OpenWrt/SUPERouter/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/SUPERouter/g' package/base-files/files/bin/config_generate
 
 # 修改版本号
 # sed -i 's/R22.3.3/R22.3.3定制版/g' package/lean/default-settings/files/zzz-default-settings
