fd5424c05ab5d3fe3625a9990a3d0c0cc5d34ebd
super27036 <144255894+super27036@users.noreply.github.com>
2023-10-11 23:38:47 +0800

    Update super-test.sh

--- scripts/super-test.sh
+++ scripts/super-test.sh
@@ -22,14 +22,14 @@ svn export https://github.com/kenzok8/small-package/trunk/luci-app-fileassistant
 rm -rf ../../customfeeds/luci/applications/luci-app-guest-wifi
 svn export https://github.com/kenzok8/small-package/trunk/luci-app-guest-wifi
 #Onliner (need luci-app-nlbwmon)
-# svn export https://github.com/rufengsuixing/luci-app-onliner/trunk luci-app-onliner
+svn export https://github.com/rufengsuixing/luci-app-onliner/trunk luci-app-onliner
 #Eqos
-# svn export https://github.com/kenzok8/small-package/trunk/luci-app-eqos
+svn export https://github.com/kenzok8/small-package/trunk/luci-app-eqos
 #Wolplus
-# svn export https://github.com/kenzok8/small-package/trunk/luci-app-wolplus
+svn export https://github.com/kenzok8/small-package/trunk/luci-app-wolplus
 
 # 存储相关应用
-# svn export https://github.com/immortalwrt/luci/trunk/applications/luci-app-gowebdav
+svn export https://github.com/immortalwrt/luci/trunk/applications/luci-app-gowebdav
 
 # 科学上网和代理应用
 #SSR
@@ -48,13 +48,13 @@ cp -rf temp/luci-app-openclash/* luci-app-openclash
 
 # 去广告
 #ADGuardHome
-# rm -rf ../../customfeeds/packages/utils/adguardhome
-# rm -rf ../../customfeeds/luci/applications/luci-app-adguardhome
-# svn export https://github.com/kenzok8/openwrt-packages/trunk/adguardhome
-# svn export https://github.com/kenzok8/openwrt-packages/trunk/luci-app-adguardhome
-# svn export https://github.com/Siriling/OpenWRT-MyConfig/trunk/configs/lede/general/applications/luci-app-adguardhome temp/luci-app-adguardhome
-# cp -rf temp/luci-app-adguardhome/* luci-app-adguardhome
-# sed -i 's/默认账号和密码均为：admin/默认用户名和密码均为root/' luci-app-adguardhome/po/zh-cn/AdGuardHome.po
+rm -rf ../../customfeeds/packages/utils/adguardhome
+rm -rf ../../customfeeds/luci/applications/luci-app-adguardhome
+svn export https://github.com/kenzok8/openwrt-packages/trunk/adguardhome
+svn export https://github.com/kenzok8/openwrt-packages/trunk/luci-app-adguardhome
+svn export https://github.com/Siriling/OpenWRT-MyConfig/trunk/configs/lede/general/applications/luci-app-adguardhome temp/luci-app-adguardhome
+cp -rf temp/luci-app-adguardhome/* luci-app-adguardhome
+sed -i 's/默认账号和密码均为：admin/默认用户名和密码均为root/' luci-app-adguardhome/po/zh-cn/AdGuardHome.po
 #dnsfilter
 # svn export https://github.com/kenzok8/small-package/trunk/luci-app-dnsfilter
 #ikoolproxy
@@ -86,23 +86,23 @@ cp -rf temp/luci-app-openclash/* luci-app-openclash
 # svn export https://github.com/kenzok8/small-package/trunk/luci-app-ddnsto
 
 # iStore应用
-# svn export https://github.com/kenzok8/small-package/trunk/taskd
-# svn export https://github.com/kenzok8/small-package/trunk/luci-lib-taskd
-# svn export https://github.com/kenzok8/small-package/trunk/luci-lib-xterm
-# svn export https://github.com/kenzok8/small-package/trunk/luci-app-store
-# svn export https://github.com/kenzok8/small-package/trunk/quickstart
-# svn export https://github.com/kenzok8/small-package/trunk/luci-app-quickstart
+svn export https://github.com/kenzok8/small-package/trunk/taskd
+svn export https://github.com/kenzok8/small-package/trunk/luci-lib-taskd
+svn export https://github.com/kenzok8/small-package/trunk/luci-lib-xterm
+svn export https://github.com/kenzok8/small-package/trunk/luci-app-store
+svn export https://github.com/kenzok8/small-package/trunk/quickstart
+svn export https://github.com/kenzok8/small-package/trunk/luci-app-quickstart
 
 # 其他
 #Socat
-# rm -rf ../../customfeeds/luci/applications/luci-app-socat
-# svn export https://github.com/kenzok8/small-package/trunk/luci-app-socat
+rm -rf ../../customfeeds/luci/applications/luci-app-socat
+svn export https://github.com/kenzok8/small-package/trunk/luci-app-socat
 #Unblockneteasemusic
-# rm -rf ../../customfeeds/luci/applications/luci-app-unblockmusic
-# svn export https://github.com/kenzok8/openwrt-packages/trunk/UnblockNeteaseMusic
-# svn export https://github.com/kenzok8/openwrt-packages/trunk/luci-app-unblockneteasemusic
+rm -rf ../../customfeeds/luci/applications/luci-app-unblockmusic
+svn export https://github.com/kenzok8/openwrt-packages/trunk/UnblockNeteaseMusic
+svn export https://github.com/kenzok8/openwrt-packages/trunk/luci-app-unblockneteasemusic
 #OpenAppFilter
-# svn export https://github.com/destan19/OpenAppFilter/trunk OpenAppFilter
+svn export https://github.com/destan19/OpenAppFilter/trunk OpenAppFilter
 
 # 网络接口
 #Minieap
@@ -110,7 +110,7 @@ svn export https://github.com/kenzok8/small-package/trunk/luci-proto-minieap
 # svn export https://github.com/kenzok8/small-package/trunk/luci-app-minieap
 
 # Apk (Apk Packages Manager)
-# svn export https://github.com/openwrt/packages/trunk/utils/apk
+svn export https://github.com/openwrt/packages/trunk/utils/apk
 
 # 主题
 git clone https://github.com/DHDAXCW/theme
@@ -144,26 +144,30 @@ popd
 #svn export https://github.com/openwrt/openwrt/trunk/package/network/services/hostapd package/network/services/hostapd
 
 # 修复移远PCIe驱动(quectel_MHI)
-# rm -rf package/wwan/driver/quectel_MHI
-# svn export https://github.com/Siriling/5G-Modem-Support/trunk/quectel_MHI package/wwan/driver/quectel_MHI
+rm -rf package/wwan/driver/quectel_MHI
+svn export https://github.com/Siriling/5G-Modem-Support/trunk/quectel_MHI package/wwan/driver/quectel_MHI
 
 #修复IPV6下发问题
-# rm -rf customfeeds/luci/applications/luci-app-hypermodem
-# svn export https://github.com/Siriling/5G-Modem-Support/trunk/luci-app-hypermodem package/community/luci-app-hypermodem
+rm -rf customfeeds/luci/applications/luci-app-hypermodem
+svn export https://github.com/Siriling/5G-Modem-Support/trunk/luci-app-hypermodem package/community/luci-app-hypermodem
 
 # 5G模组短信插件
-# rm -rf customfeeds/package/utils/sms-tool
-# rm -rf customfeeds/luci/applications/luci-app-sms-tool
-# svn export https://github.com/dwj0/luci-app-sms-tool/trunk/sms-tool package/community/sms-tool
-# svn export https://github.com/dwj0/luci-app-sms-tool/trunk/luci-app-sms-tool package/community/luci-app-sms-tool
-# svn export https://github.com/Siriling/OpenWRT-MyConfig/trunk/configs/general/applications/luci-app-sms-tool temp/luci-app-sms-tool
-# cp -rf temp/luci-app-sms-tool/* package/community/luci-app-sms-tool
+rm -rf customfeeds/package/utils/sms-tool
+rm -rf customfeeds/luci/applications/luci-app-sms-tool
+svn export https://github.com/dwj0/luci-app-sms-tool/trunk/sms-tool package/community/sms-tool
+svn export https://github.com/dwj0/luci-app-sms-tool/trunk/luci-app-sms-tool package/community/luci-app-sms-tool
+svn export https://github.com/Siriling/OpenWRT-MyConfig/trunk/configs/general/applications/luci-app-sms-tool temp/luci-app-sms-tool
+cp -rf temp/luci-app-sms-tool/* package/community/luci-app-sms-tool
 
 # 5G模组拨号脚本
-# mkdir -p package/base-files/files/root/5GModem
-# cp -rf $GITHUB_WORKSPACE/tools/5G模组拨号脚本/5GModem/* package/base-files/files/root/5GModem
-# chmod -R a+x package/base-files/files/root/5GModem
-# svn export https://github.com/Siriling/OpenWRT-MyConfig/trunk/configs/general/etc/crontabs package/base-files/files/etc/crontabs
+mkdir -p package/base-files/files/root/5GModem
+cp -rf $GITHUB_WORKSPACE/tools/5G模组拨号脚本/5GModem/* package/base-files/files/root/5GModem
+chmod -R a+x package/base-files/files/root/5GModem
+svn export https://github.com/Siriling/OpenWRT-MyConfig/trunk/configs/general/etc/crontabs package/base-files/files/etc/crontabs
+
+# Frp、Openclash、Argon设置
+cp -rf $GITHUB_WORKSPACE/PATCH/R2S/files/etc/* package/base-files/files/root/etc
+chmod -R a+x package/base-files/files/root/etc
 
 # 修改默认IP地址
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
@@ -182,12 +186,12 @@ sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/$1$S2TRFyMU$E8fE0RRK
 # sed -i 's/OpenWrt/R5S/g' package/lean/default-settings/files/zzz-default-settings
 
 # 系统信息内添加编译者信息
-# sed -i '/<tr><td width="33%"><%:CPU usage (%)%><\/td><td id="cpuusage">-<\/td><\/tr>/a\<tr><td width="33%"><%:Compiler author%><\/td><td>Siriling<\/td><\/tr><tr><td width="33%"><%:Resources link%><\/td><td><a target="_blank" style="padding-right: 20px;" href="https://github.com/Siriling/OpenWRT_x86_x64">固件仓库<\/a><a target="_blank" style="padding-left: 20px;padding-right: 20px;" href="https://blog.siriling.com:1212/2023/03/18/openwrt-5g-modem">模组教程<\/a><\/td><\/tr>' package/lean/autocore/files/x86/index.htm
-# sed -i '/<tr><td width="33%"><%:CPU usage (%)%><\/td><td id="cpuusage">-<\/td><\/tr>/a\<tr><td width="33%"><%:Compiler author%><\/td><td>Siriling<\/td><\/tr><tr><td width="33%"><%:Resources link%><\/td><td><a target="_blank" style="padding-right: 20px;" href="https://github.com/Siriling/OpenWRT_x86_x64">固件仓库<\/a><a target="_blank" style="padding-left: 20px;padding-right: 20px;" href="https://blog.siriling.com:1212/2023/03/18/openwrt-5g-modem">模组教程<\/a><\/td><\/tr>' package/lean/autocore/files/arm/index.htm
+sed -i '/<tr><td width="33%"><%:CPU usage (%)%><\/td><td id="cpuusage">-<\/td><\/tr>/a\<tr><td width="33%"><%:Compiler author%><\/td><td>Siriling<\/td><\/tr><tr><td width="33%"><%:Resources link%><\/td><td><a target="_blank" style="padding-right: 20px;" href="https://github.com/Siriling/OpenWRT_x86_x64">固件仓库<\/a><a target="_blank" style="padding-left: 20px;padding-right: 20px;" href="https://blog.siriling.com:1212/2023/03/18/openwrt-5g-modem">模组教程<\/a><\/td><\/tr>' package/lean/autocore/files/x86/index.htm
+sed -i '/<tr><td width="33%"><%:CPU usage (%)%><\/td><td id="cpuusage">-<\/td><\/tr>/a\<tr><td width="33%"><%:Compiler author%><\/td><td>Siriling<\/td><\/tr><tr><td width="33%"><%:Resources link%><\/td><td><a target="_blank" style="padding-right: 20px;" href="https://github.com/Siriling/OpenWRT_x86_x64">固件仓库<\/a><a target="_blank" style="padding-left: 20px;padding-right: 20px;" href="https://blog.siriling.com:1212/2023/03/18/openwrt-5g-modem">模组教程<\/a><\/td><\/tr>' package/lean/autocore/files/arm/index.htm
 
 # 给添加的代码添加汉化
-# sed -i '$a\\nmsgid "Compiler author"\nmsgstr "编译作者"' feeds/luci/modules/luci-base/po/zh-cn/base.po
-# sed -i '$a\\nmsgid "Resources link"\nmsgstr "资源链接"' feeds/luci/modules/luci-base/po/zh-cn/base.po
+sed -i '$a\\nmsgid "Compiler author"\nmsgstr "编译作者"' feeds/luci/modules/luci-base/po/zh-cn/base.po
+sed -i '$a\\nmsgid "Resources link"\nmsgstr "资源链接"' feeds/luci/modules/luci-base/po/zh-cn/base.po
 
 # 更换内核版本
 # sed -i 's/6.1/5.15/g' target/linux/x86/Makefile
@@ -196,14 +200,14 @@ sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/$1$S2TRFyMU$E8fE0RRK
 sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 
 # 修改banner
-# rm -rf package/base-files/files/etc/banner
-# wget -P package/base-files/files/etc https://raw.githubusercontent.com/DHDAXCW/lede-rockchip/stable/package/base-files/files/etc/banner
+rm -rf package/base-files/files/etc/banner
+wget -P package/base-files/files/etc https://raw.githubusercontent.com/DHDAXCW/lede-rockchip/stable/package/base-files/files/etc/banner
 
 # Fix libssh
-# pushd feeds/packages/libs
-# rm -rf libssh
-# svn export https://github.com/openwrt/packages/trunk/libs/libssh
-# popd
+pushd feeds/packages/libs
+rm -rf libssh
+svn export https://github.com/openwrt/packages/trunk/libs/libssh
+popd
 
 # 编译新版Sing-box和hysteria，需golang版本1.20或者以上版本
 pushd feeds/packages/lang
